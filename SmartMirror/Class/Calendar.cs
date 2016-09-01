4df238e966c1e53c2027fa8612ef799e58b7fc5d
Sven <glaglasven@live.com>
2016-09-01 13:00:56 +0200

    First correction to XAML and Speech #1

---
+++ SmartMirror/Class/Calendar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media;
+
+namespace SmartMirror
+{
+	class Calendar
+	{
+		public int Number { get; set; }
+		public double EventName { get; set; }
+		public SolidColorBrush color { get; set; }
+
+		public Calendar()
+		{
+
+		}
+	}
+}
---
+++ SmartMirror/Class/Weather.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartMirror
+{
+	class Weather
+	{
+		public string Day { get; set; }
+		public double Temp { get; set; }
+		public string Icon { get; set; }
+
+		public Weather(string day, double temp, string icon)
+		{
+			Day = day;
+			Temp = temp;
+			Icon = icon;
+		}
+	}
+}
