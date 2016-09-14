1086f04fc97ecfcc64955b229935acdeabcb8925
Sven <glaglasven@live.com>
2016-09-14 21:20:22 +0200

    #emotion

---
+++ SmartMirror/Class/ComputerVision/Emotional.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Emotion.Contract;
+using System.IO;
+
+namespace SmartMirror.Class.ComputerVision
+{
+	class Emotional
+	{
+		public static async Task<Emotion[]> GetEmotionFromFile(Stream imageFileStream)
+		{
+			EmotionServiceClient emotionServiceClient = new EmotionServiceClient("b31e347bbb0241a3a1d20f87623d91ca");
+			try
+			{
+				Emotion[] emotionResult;
+				emotionResult = await emotionServiceClient.RecognizeAsync(imageFileStream);
+				return emotionResult;
+			}
+			catch (Exception exception)
+			{
+				return null;
+			}
+		}
+	}
+}
--- SmartMirror/Class/News/NewsData.cs
+++ SmartMirror/Class/News/NewsData.cs
@@ -1,23 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SmartMirror.Class.News
+﻿namespace SmartMirror.Class.News
 {
 	class NewsData
 	{
+		/// <summary>
+		/// News Title
+		/// </summary>
 		public string Title { get; set; }
+
+		/// <summary>
+		/// News Description
+		/// </summary>
 		public string Description { get; set; }
+
+		/// <summary>
+		/// News AAuthor
+		/// </summary>
 		public string Author { get; set; }
+
+		/// <summary>
+		/// News link
+		/// </summary>
 		public string Url { get; set; }
+
+		/// <summary>
+		/// Image url of news
+		/// </summary>
 		public string ImageUrl { get; set; }
+
+		/// <summary>
+		/// Date of publication
+		/// </summary>
 		public string Date { get; set; }
 
 		public NewsData()
 		{
-
 		}
 	}
 }
